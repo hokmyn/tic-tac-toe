b872097ef3ef5a14e18e69a8edcea6edb97afb5f
hokmyn <den_from_bykhov1988@mail.ru>
2021-08-18 18:44:51 +0300

    feat: add solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,97 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.prevSymb = "";
+    this.arr = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    this.curSymb = "x";
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.curSymb;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.getFieldValue(rowIndex, columnIndex)) {
+      this.arr[rowIndex][columnIndex] = this.curSymb;
+      this.prevSymb = this.curSymb;
+      this.curSymb = this.curSymb === "x" ? "o" : "x";
+    }
+  }
+
+  isFinished() {
+    return this.getWinner() || this.isDraw() ? true : false;
+  }
+
+  getWinner() {
+    let isRowCompl = false;
+    let isColCompl = false;
+    let isMainDiagCompl = false;
+    let isSideDiagCompl = false;
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.arr[i].every((elem) => elem === this.prevSymb)) {
+        isRowCompl = true;
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (this.arr[i][i] === this.prevSymb) {
+        count++;
+      }
+    }
+    if (count === 3) {
+      isMainDiagCompl = true;
+    } else {
+      count = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+      if (this.arr[i][this.arr.length - i - 1] === this.prevSymb) {
+        count++;
+      }
+    }
+    if (count === 3) {
+      isSideDiagCompl = true;
+    } else {
+      count = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.arr[j][i] === this.prevSymb) {
+          count++;
+        }
+      }
+      if (count === 3) {
+        isColCompl = true;
+      } else {
+        count = 0;
+      }
     }
 
-    getCurrentPlayerSymbol() {
-
+    if (isRowCompl || isSideDiagCompl || isColCompl || isMainDiagCompl) {
+      return this.prevSymb;
+    } else {
+      return null;
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
-
+  noMoreTurns() {
+    for (let i = 0; i < 3; i++) {
+      if (this.arr[i].indexOf(null) !== -1) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    isFinished() {
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner() ? true : false;
+  }
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.arr[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
